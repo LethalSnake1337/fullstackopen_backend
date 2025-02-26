e0792e6ac622b731f2166f3b01ad3d56f8722078
Christos <christos.labroussis@campus.tu-berlin.de>
2025-02-26 12:45:56 +0100

    Exercise 3.6

--- index.js
+++ index.js
@@ -56,6 +56,13 @@ app.post("/api/persons", (req, res) => {
             error: "name or number missing"
         });
     }
+    
+    if (persons.find(person => person.name === body.name)) {
+        return res.status(400).json({
+            error: "name must be unique"
+        });
+    }
+
     const person = {
         name: body.name,
         number: body.number,
