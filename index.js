a8378e8358f93b9495277020b13cbd6f97020221
Christos <christos.labroussis@campus.tu-berlin.de>
2025-02-28 08:43:17 +0100

    Exercise 3.15-3.18

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
+const Phonebook = require("./models/phonebook");
 
 app.use(express.json());
 app.use(cors());
@@ -10,57 +12,47 @@ app.use(express.static("dist"));
 morgan.token("body", (req) => JSON.stringify(req.body));
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message);
+
+    if (error.name === "CastError") {
+        return res.status(400).send({ error: "malformatted id" });
     }
-]
+    next(error);
+}
+
+app.use(errorHandler)
+
 
 app.get("/api/persons", (req, res) => {
-    res.json(persons);
+    Phonebook.find({}).then(result => {
+        res.json(result)
+    })
 });
 
 app.get("/info", (req, res) => {
     const date = new Date();
-    res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`);
+    Phonebook.find({}).then(persons => {
+        res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`);
+    }
+    )
 });
 
-app.get("/api/persons/:id", (req, res) => {
-    const id = req.params.id;
-    const person = persons.find(person => person.id === id);
-    if (person) {
-        res.json(person);
-    } else {
-        res.status(404).end();
-    }
+app.get("/api/persons/:id", (req, res, next) => {
+    Phonebook.findById(req.params.id).then(result => {
+        if (result) {
+            res.json(result);
+        }
+        else {
+            res.status(404).end();
+        }
+    }).catch(error => next(error));
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-    console.log(persons);
-    
-    const id = req.params.id;
-    persons = persons.filter(person => person.id !== id);
-    console.log(persons);
-    
-    res.status(204).end();
-    
+    Phonebook.findByIdAndDelete(req.params.id).then(result => {
+        res.status(204).end();
+    }).catch(error => next(error));
 });
 
 app.post("/api/persons", (req, res) => {
@@ -71,19 +63,35 @@ app.post("/api/persons", (req, res) => {
         });
     }
 
-    if (persons.find(person => person.name === body.name)) {
-        return res.status(400).json({
-            error: "name must be unique"
-        });
-    }
+    Phonebook.findOne({ name: body.name }).then(result => {
+        if (result) {
+            existingPerson.number = body.number;
+            existingPerson.save().then(updatedPerson => {
+                res.json(updatedPerson)}).catch(error => next(error));
+        } else {
+            const person = new Phonebook({
+                name: body.name,
+                number: body.number,
+                id: String(Math.floor(Math.random() * 1000))
+            });
+            person.save().then(result => {
+                res.json(result);
+            })
+        }
+    });
+});
 
+app.put("/api/persons/:id", (req, res) => {
+    const body = req.body;
     const person = {
         name: body.name,
-        number: body.number,
-        id: String(Math.floor(Math.random() * 1000))
+        number: body.number
     };
-    persons = persons.concat(person);
-    res.json(person);
+
+    Phonebook.findByIdAndUpdate(req.params.id, person, { new: true })
+        .then(result => {
+            res.json(result);
+        }).catch(error => next(error));
 });
 
 const PORT = process.env.PORT || 3001;
